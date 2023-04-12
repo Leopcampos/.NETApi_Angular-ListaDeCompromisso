e6a4d65f561cc1a86d947e1af102ab985857378a
Leo Campos <leopcampos85@gmail.com>
2023-04-12 19:15:56 -0300

    Criação do projeto

---
+++ src/ProjetoApiEntittyFramework.Domain/Entities/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoApiEntittyFramework.Domain.Entities
+{
+    public class Usuario
+    {
+        public Guid IdUsuario { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public DateTime DataCriacao { get; set; }
+
+        #region Relacionamentos
+
+        public List<Compromisso> Compromissos { get; set; }
+
+        #endregion
+    }
+}

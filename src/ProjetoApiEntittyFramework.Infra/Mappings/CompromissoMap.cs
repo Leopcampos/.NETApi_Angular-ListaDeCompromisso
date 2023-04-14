b33d33b28293c43c8dcbcbf5c88fcfa3a03a7fd7
Leo Campos <leopcampos85@gmail.com>
2023-04-13 21:09:13 -0300

    Aula 17 parte 1

---
+++ src/ProjetoApiEntittyFramework.Infra/Mappings/CompromissoMap.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProjetoApiEntittyFramework.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoApiEntittyFramework.Infra.Mappings
+{
+    public class CompromissoMap : IEntityTypeConfiguration<Compromisso>
+    {
+        public void Configure(EntityTypeBuilder<Compromisso> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

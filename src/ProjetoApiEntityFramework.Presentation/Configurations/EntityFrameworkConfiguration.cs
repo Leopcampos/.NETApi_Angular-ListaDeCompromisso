225528b2bfc7e08a16d3cbd006ea8432669ea12b
Leo Campos <leopcampos85@gmail.com>
2023-04-15 12:35:57 -0300

    Aula 18 Finalizada

---
+++ src/ProjetoApiEntityFramework.Presentation/Configurations/EntityFrameworkConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoApiEntittyFramework.Infra.Contexts;
+using ProjetoApiEntittyFramework.Infra.Contracts;
+using ProjetoApiEntittyFramework.Infra.Repositories;
+
+namespace ProjetoApiEntityFramework.Presentation.Configurations
+{
+    public static class EntityFrameworkConfiguration
+    {
+        public static void AddEntityFrameworkConfiguration(this WebApplicationBuilder builder)
+        {
+            #region Configuração do Contexto do EntityFramework
+
+            builder.Services.AddDbContext<SqlServerContext>(options =>
+                           options.UseSqlServer(builder.Configuration.GetConnectionString("Context")));
+
+            #endregion
+
+            #region Configuração das interfaces e classes de repositório
+
+            builder.Services.AddTransient<IUsuarioRepository, UsuarioRepository>();
+            builder.Services.AddTransient<ICompromissoRepository, CompromissoRepository>();
+
+            #endregion
+        }
+    }
+}
---
+++ src/ProjetoApiEntityFramework.Presentation/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoApiEntittyFramework.Infra.Contracts;
+using ProjetoApiEntityFramework.Presentation.Models;
+using ProjetoApiEntityFramework.Presentation.Settings;
+
+namespace ProjetoApiEntityFramework.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post(LoginModel model,
+            [FromServices] IUsuarioRepository usuarioRepository,
+            [FromServices] TokenSettings tokenSettings)
+        {
+            try
+            {
+                //verificar se o email e senha pertencem a algum usuario..
+                var usuario = usuarioRepository.Get(model.Email, model.Senha);
+
+                if (usuario == null) //se usuário não foi encontrado
+                    return StatusCode(401, "Acesso negado. Usuário inválido.");
+
+                //gerando e retornando o TOKEN de autenticação..
+                return Ok(new
+                {
+                    Message = "Usuário autenticado com sucesso.",
+                    AccessToken = tokenSettings.GenerateToken(usuario.Email)
+                }); ;
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
